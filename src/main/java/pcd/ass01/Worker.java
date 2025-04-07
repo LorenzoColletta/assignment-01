0d762cf5b8bd5392077baa4d13d32ecc73e62b5d
LorenzoColletta <lorenzo.colletta@studio.unibo.it>
2025-04-07 10:51:15 +0200

    feat: implements worker class

---
+++ src/main/java/pcd/ass01/Worker.java
@@ -0,0 +1,44 @@
+package pcd.ass01;
+
+import java.util.List;
+
+public class Worker extends Thread{
+    private List<Boid> boids;
+    private BoidsModel model;
+    private SynchWorkersView viewBarrier;
+    private SynchWorkers positionBarrier;
+
+    Worker(List<Boid> boids, BoidsModel model, SynchWorkersView viewBarrier, SynchWorkers positionBarrier){
+        this.boids = boids;
+        this.model = model;
+        this.viewBarrier = viewBarrier;
+        this.positionBarrier = positionBarrier;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        while (true){
+            for (Boid boid : boids) {
+                boid.updateVelocity(model);
+            }
+            //System.out.println("Velocity Updated");
+            try {
+                positionBarrier.notifyJobDone();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            //System.out.println("Velocity wait terminated");
+            for (Boid boid : boids) {
+                boid.updatePos(model);
+            }
+            //System.out.println("Position Updated");
+            try {
+                viewBarrier.notifyJobDone();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            //System.out.println("View Update wait terminated");
+        }
+    }
+}
