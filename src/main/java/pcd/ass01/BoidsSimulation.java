a8b9f3294e5f6e9f37b9e0f5643fd2ef7ff89d43
LorenzoColletta <lorenzo.colletta@studio.unibo.it>
2025-04-10 16:51:56 +0200

    add: multiple run

--- src/main/java/pcd/ass01/BoidsSimulation.java
+++ src/main/java/pcd/ass01/BoidsSimulation.java
@@ -1,12 +1,12 @@
 package pcd.ass01;
 
 public class BoidsSimulation {
+	private	static final int N_CORES = 4;
 
 	private static final int N_BOIDS_SIM_1 = 1500;
 	private static final int N_FRAMES = 1500;
 	private static final int N_WORKERS = 1;
 
-	final static int N_BOIDS = 1500;
 
 	final static double SEPARATION_WEIGHT = 1.0;
     final static double ALIGNMENT_WEIGHT = 1.0;
@@ -20,11 +20,12 @@ public class BoidsSimulation {
 
 	final static int SCREEN_WIDTH = 800; 
 	final static int SCREEN_HEIGHT = 800; 
-	
 
-    public static void main(String[] args) {      
+
+    public static void main(String[] args) {
+
     	var model = new BoidsModel(
-    					N_BOIDS, 
+    					N_BOIDS_SIM_1,
     					SEPARATION_WEIGHT, ALIGNMENT_WEIGHT, COHESION_WEIGHT, 
     					ENVIRONMENT_WIDTH, ENVIRONMENT_HEIGHT,
     					MAX_SPEED,
@@ -34,10 +35,24 @@ public class BoidsSimulation {
 //    	var view = new BoidsView(sim, SCREEN_WIDTH, SCREEN_HEIGHT);
 //    	var view = new BasicView(sim);
 //    	sim.attachView(view);
-		var t0 = System.currentTimeMillis();
-		sim.startSimulation(N_BOIDS_SIM_1, N_WORKERS, N_FRAMES);
-    	sim.run();
-		var t1 = System.currentTimeMillis();
-		System.out.println("Elapsed time: " + (t1 - t0));
+
+		for(int i = 1; i < N_CORES + 1; i++){
+			var t0 = System.currentTimeMillis();
+			sim.startSimulation(N_BOIDS_SIM_1, i, N_FRAMES);
+			sim.run();
+			var t1 = System.currentTimeMillis();
+			System.out.println("N_BOIDS: " + N_BOIDS_SIM_1 + " N_FRAMES: " + N_FRAMES + " N_core: " + i + " Elapsed time: " + (t1 - t0));
+		}
+
+//		for(int i = 1; i < N_CORES + 1; i++){
+//			var t0 = System.currentTimeMillis();
+//			int nBoids = (int)(Math.cbrt(i) * N_BOIDS_SIM_1);
+//			sim.startSimulation(nBoids, i, N_FRAMES);
+//			sim.run();
+//			var t1 = System.currentTimeMillis();
+//			System.out.println("N_BOIDS: " + nBoids + " N_FRAMES: " + N_FRAMES + " N_core: " + i + " Elapsed time: " + (t1 - t0));
+//		}
+
+
     }
 }
